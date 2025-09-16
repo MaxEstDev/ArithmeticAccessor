bfb784b0941a371549d9149b4c66b31b569160e8
MaxEstLa <96690800+MaxEstDev@users.noreply.github.com>
2025-09-17 00:14:43 +0200

    Added new compact functions declarations

--- Source/VectorAccessor/Public/AA_Vector3AccessorLibrary.h
+++ Source/VectorAccessor/Public/AA_Vector3AccessorLibrary.h
@@ -13,4 +13,12 @@ UCLASS()
 class VECTORACCESSOR_API UAA_Vector3AccessorLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "VectorAccessor", meta =(CompactNodeTitle))
+	static int32 VectorToCompact(const FVector Vector);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "VectorAccessor", meta =(CompactNodeTitle))
+	static FVector CompactToVector(int32 Compact);
 };
